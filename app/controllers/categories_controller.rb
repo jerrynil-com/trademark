f1613d26ae4d4335a0fc6507293bcd6dbcfc8a78
jerrynil <jerrynil.tw@gmail.com>
2017-10-07 20:24:12 +0800

    divide items by uniq_group_codes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,5 +5,6 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @uniq_group_codes = @category.items.pluck(:group_code).uniq
   end
 end
